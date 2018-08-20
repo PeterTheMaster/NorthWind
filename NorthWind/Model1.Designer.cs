e6524f1e31e0e905f9269908cf186dba85047515
Pedro José Rodríguez López <pedro.jose.rodriguez.lopez@everis.com>
2018-08-20 09:11:03 +0200

    Borrado modelo prueba

--- NorthWind/Model1.Designer.cs
+++ NorthWind/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'L:\DRIVE\GIT_REPO\NorthWindTest\NorthWind\Prueba01Modelo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\prodrilo\Documents\DRIVE\OneDrive - everis\GIT\NorthWing\NorthWind\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
